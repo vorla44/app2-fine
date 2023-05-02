95839564a6ac1e695b277841e5b0db4b3b97c1e6
Veikko Rintala <veikko0912@gmail.com>
2023-05-02 06:59:41 +0300

    228 Send Email Via the Web Form

--- send_email.py
+++ send_email.py
@@ -1,11 +1,14 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "vrpython44@gmail.com"
-    password = "nbrbypizodtjopxf"
+    password = os.getenv("PASSWORD")
+    #password = "nbrbypizodtjopxf"
+
     receiver = "vrpython44@gmail.com"
     context = ssl.create_default_context()
 
