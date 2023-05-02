b4388f7ba465d4a080bcf666defe64bd3eb0df56
Veikko Rintala <veikko0912@gmail.com>
2023-05-02 05:27:05 +0300

    Day 23 Send email

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "vrpython44@gmail.com"
+password ="nbrbypizodtjopxf"
+
+receiver = "vrpython44@gmail.com"
+context = ssl.create_default_context()
+
+message = """\
+Subject: Hi!
+How are you?
+Bye!
+"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
